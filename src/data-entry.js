dff4eba4db34373a8b32f146dc802fab55892a5c
Doppen <basdoppen@gmail.com>
2018-07-10 08:03:27 +0200

    Data entry

--- src/data-entry.js
+++ src/data-entry.js
@@ -24,16 +24,122 @@ const Fragment = React.Fragment;
       );
     }
   }
-  export class MolecSearch extends React.Component {
+  export class FromDescription extends React.Component {
     render() {
       return (
-        <Fragment>
-          <div className="huc-form-element">
-            <AtomImput />
-            <AtomButton />
-          </div>
-        </Fragment>
+        <div className="huc-form-description">
+          {this.props.description}
+        </div>
       );
     }
   }
-  
+
+
+export class HcSearch extends React.Component {
+  render() {
+    return (
+      <div className="huc-form-element">
+        <label htmlFor={this.props.name} className="huc-form-label">{this.props.label}</label>
+        <FromDescription description={this.props.description} />
+        <div className="huc-connect-elements">
+          <input type="text" name={this.props.name} placeholder={this.props.placeholder} />
+          <button className="button icon iconDate"></button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export class HcFormInput extends React.Component {
+  render() {
+    return (
+      <div className="huc-form-element">
+        <label htmlFor={this.props.name} className="huc-form-label">{this.props.label}</label>
+        <FromDescription description={this.props.description} />
+        <input type="text" name={this.props.name} placeholder={this.props.placeholder} />
+      </div>
+    );
+  }
+}
+
+
+
+export class HcFormImage extends React.Component {
+  render() {
+    return (
+      <div className="huc-form-element">
+        <label htmlFor={this.props.name} className="huc-form-label">{this.props.label}</label>
+        <FromDescription description={this.props.description} />
+        <input type="file" name={this.props.name}  />
+      </div>
+    );
+  }
+}
+
+export class HcFormDate extends React.Component {
+  render() {
+    return (
+      <div className="huc-form-element">
+        <label htmlFor={this.props.name} className="huc-form-label">{this.props.label}</label>
+        <FromDescription description={this.props.description} />
+        <div className="huc-connect-elements">
+          <input type="text" name={this.props.name} placeholder={this.props.placeholder} />
+          <button className="button icon iconDate"></button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export class HcFormPerson extends React.Component {
+  render() {
+    return (
+      <div className="huc-form-element">
+        <div className="huc-form-label">Person</div>
+        <FromDescription description={this.props.description} />
+        <div className="huc-fieldgroup">
+          <div className="huc-fieldgroup-item">
+            <label htmlFor={this.props.name1} className="huc-form-label">First name</label>
+            <input type="text" name={this.props.name1} />
+          </div>
+
+          <div className="huc-fieldgroup-item">
+            <label htmlFor={this.props.name2} className="huc-form-label">Last name</label>
+            <input type="text" name={this.props.name2} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export class HcFormCoordinats extends React.Component {
+  render() {
+    return (
+      <div className="huc-form-element">
+        <div className="huc-form-label">Coordinates</div>
+        <FromDescription description={this.props.description} />
+        <div className="huc-fieldgroup">
+          <div className="huc-fieldgroup-item">
+            <label htmlFor={this.props.name1} className="huc-form-label">Longitude</label>
+            <input type="text" name={this.props.name1} />
+          </div>
+
+          <div className="huc-fieldgroup-item">
+            <label htmlFor={this.props.name2} className="huc-form-label">Latitude</label>
+            <input type="text" name={this.props.name2} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export class HcFormItemBrowser extends React.Component {
+  render() {
+    return (
+      <Fragment>
+      </Fragment>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {StyleGuide, DescribedMock, Embed} from './component-view';
-  import { AtomImput } from './data-entry';
-  import { AtomButton } from './data-entry';
-  import { MolecSearch } from './data-entry';
+// eslint-disable-next-line
+import { HcSearch,HcFormInput,HcFormImage,HcFormDate,HcFormPerson,HcFormCoordinats,HcFormItemBrowser } from './data-entry';
 const Fragment = React.Fragment;
 
 
@@ -16,30 +15,55 @@ ReactDOM.render(
     <StyleGuide>
       <DescribedMock title="Data entry">
         Components for data entry, forms and uploads.
-    
-          <Embed caption="Basic input" description="A simple imput field">
-            <AtomImput
-          
-              type="text"
-              name="FamilyName"
-              placeholder="Jansen"
-            />
-          </Embed>
-          <Embed caption="Basic button" description="A simple button">
-            <AtomButton
-          
-              label="Save"
-            />
-          </Embed>
-          <Embed caption="Search field" description="A simple search field">
-            <MolecSearch
-          
-              type="text"
+
+          <Embed caption="Search field" description="">
+            <HcSearch
+              description="text"
               name="Text search"
               label="Search"
+              placeholder="Example: Huygens"
             />
           </Embed>
-    
+
+          <Embed caption="Basic input" description="">
+            <HcFormInput
+              description="Fill in the first part of your name"
+              name="firstName"
+              label="First name"
+              placeholder="Bas Doppen"
+            />
+          </Embed>
+
+          <Embed caption="Image field" description="">
+            <HcFormImage
+              description="You can use .png, .jpg or .svg"
+              name="firstName"
+              label="Select an avatar"
+            />
+          </Embed>
+
+          <Embed caption="Date field" description="">
+            <HcFormDate
+              description=""
+              name="date"
+              label="Date"
+            />
+          </Embed>
+
+
+          <Embed caption="Person name field" description="">
+            <HcFormPerson
+              description="Fill in the name of the person."
+            />
+          </Embed>
+
+          <Embed caption="Coordinates field" description="">
+            <HcFormCoordinats
+              description=""
+            />
+          </Embed>
+
+
       </DescribedMock>
     </StyleGuide>
 
